55a463b29b49f10dbca346ecedd0c4b286f60876
Cassiana <lima.cglima@gmail.com>
2022-10-01 19:17:31 -0300

    alterações no css componente modal

--- script.js
+++ script.js
@@ -139,30 +139,51 @@ var todosJogos = {
             {
                 nome: "Goof Troop",
                 url: "https://pbs.twimg.com/media/Exb5jmAXEAMoktu.jpg",
+                descricao: `Goof Troop é um jogo de ação-aventura e puzzles desenvolvido pela Capcom e lançado em 1993
+                na América do Norte e Europa e em 1994 no Japão. O jogo é baseado na série de mesmo nome.
+                Antes de começar a jogar o jogador pode escolher entre ser Max ou Pateta no modo single player`,
             },
             {
                 nome: "Donkey Kong I",
                 url: "https://jumpersroms.files.wordpress.com/2019/09/donkey-kong-1-c-snes-pt-br.jpg",
+                descricao: `Donkey Kong é uma série de jogos eletrônicos da Nintendo,
+                criada por Shigeru Miyamoto que gira em torno do personagem Donkey Kong.`,
             },
             {
                 nome: "Donkey Kong II",
                 url: "https://static.emulatorgames.net/images/super-nintendo/donkey-kong-country-2-diddys-kong-quest-1-1.jpg",
+                descricao: `Donkey Kong Country 2: Diddy's Kong Quest é um jogo eletrônico de plataforma desenvolvido
+                pela Rare e publicado pela Nintendo em novembro de 1995 para o Super Nintendo Entertainment System.
+                É o segundo jogo da série Donkey Kong Country.`,
             },
             {
                 nome: "Donkey Kong III",
                 url: "https://1.bp.blogspot.com/-KlsYTIth3Dg/Xnlu5vc_kaI/AAAAAAAAAgI/iJIx3tdTvAsmY7jlqGbc3Uf5fbguX01EQCLcBGAsYHQ/s1600/DK.jpg",
+                descricao: `Donkey Kong Country 3: Dixie Kong's Double Trouble! é um jogo eletrônico de plataforma desenvolvido
+                pela Rare e publicado pela Nintendo para o Super Nintendo Entertainment System em 1996.
+                É a terceiro título da série Donkey Kong Country e serve como uma sequência direta
+                de Donkey Kong Country 2: Diddy's Kong Quest.`,
             },
             {
                 nome: "Super Mario Kart",
                 url: "https://joysticknervoso.files.wordpress.com/2017/02/super_mario_kart.jpg",
+                descricao: `Super Mario Kart é um jogo eletrônico de corrida com personagens da série Mario lançado em
+                1992 para o Super Nintendo.`,
             },
             {
                 nome: "Super Mario World",
                 url: "https://jumpersroms.files.wordpress.com/2020/08/super-mario-world-2-pt-br-c-snes.jpg?w=940",
+                descricao: `Super Mario World, originalmente chamado no Japão de Super Mario Bros. 4,
+                é um jogo eletrônico de plataforma desenvolvido pela Nintendo Entertainment Analysis & Development
+                e publicado pela Nintendo, em 1990, para o console Super Nintendo Entertainment System`,
             },
             {
                 nome: "The Legend of Zelda",
                 url: "https://jumpersroms.files.wordpress.com/2020/03/the-legend-of-zelda-a-link-to-the-past-pt-br-c-snes.jpg",
+                descricao: `É o terceiro jogo da série The Legend of Zelda, e foi lançado em 1991 no Japão e em 1992
+                na América do Norte e Europa. O lançamento foi um sucesso comercial e de crítica, sendo um marco para a Nintendo
+                e é considerado como um dos melhores jogos de todos os tempos, inclusive pelo seu enredo, e vendeu mais de
+                quatro milhões de cópias em todo mundo.`,
             },
         ],
     },
@@ -173,18 +194,23 @@ var todosJogos = {
             {
                 nome: "Crash Bandicoot",
                 url: "https://www.gamulator.com/img/roms/crash-bandicoot-psx.jpg",
+                descricao: `Crash Bandicoot é o primeiro jogo da série lançado pela Naughty Dog em 1996. O jogo se passa nas fictícias ilhas Wumpa,
+                um arquipélago situado na costa noroeste da Austrália e possui 32 fases, sendo seis delas de chefes e duas fases secretas.`,
             },
             {
                 nome: "Harvest Moon",
                 url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ--D-FkRdE6-OUWhhzS63y-gSS8s_xDLYiw&usqp=CAU",
-            },
-            {
-                nome: "Crash Bandicoot I",
-                url: "https://www.gamulator.com/img/roms/crash-bandicoot-warped-PS1-cover.jpg",
+                descricao: `Harvest Moon: Back to Nature foi o quinto jogo da franquia Harvest Moon,
+                sendo o terceiro produzido para consoles de mesa.`,
             },
             {
                 nome: "Legend Of Mana",
                 url: "https://www.gamulator.com/img/roms/legend-of-mana-ps1-cover-slus-01013-340x483.jpg",
+                descricao: `Legend of Mana é um jogo eletrônico do gênero RPG,
+                lançado no final de 2000 (1999 no Japão) para PlayStation.
+                O jogo possibilita a escolha de um personagem masculino ou feminino e possui vários personagens
+                espalhados pelo mundo do jogo que entrarão para seu grupo para que ajude nas missões dele e vice versa,
+                ademais, um segundo jogador pode se juntar ao jogo ao conectar um segundo controle e controlar estes companheiros.`,
             },
         ],
     },
@@ -195,22 +221,39 @@ var todosJogos = {
             {
                 nome: "Harvest Moon",
                 url: "https://www.gamulator.com/img/roms/harvest-moon-boy-girl-psp-cover.jpg",
+                descricao: `Sucesso nos consoles de mesa, garoto e garota descobrem as maravilhas
+                da vida no campo no portátil da Sony.`,
             },
             {
                 nome: "LocoRoco2",
                 url: "https://cache.downloadroms.io/static/da13fa03f9af224ad0eebc1e65791f742562f805/image.jpeg",
+                descricao: `ocoRoco 2 é um jogo de plataforma desenvolvido pela Japan Studio e publicado pela Sony Computer Entertainment.
+                É a sequência do LocoRoco de 2006 e foi lançado para o console portátil PSP em 2008.`,
             },
             {
                 nome: "Monster Hunter",
                 url: "https://www.gamulator.com/img/roms/monster-hunter-freedom-unite-psp-cover.jpg",
+                descricao: `Monster Hunter Freedom Unite é um game de ação e aventura,
+                que possui alguns elementos de progressão de personagem RPGs tradicionais.O objetivo é começar a carreira
+                como um caçador de monstros e desenvolver suas habilidades para conseguir eliminar
+                as mais perigosas criaturas de seu mundo. Recebendo missões que em sua maioria incluem
+                o extermínio de um determinado monstro, ao longo do caminho, o jogador vai aperfeiçoar suas técnicas
+                e seus itens de modo a completar seus objetivos e assim, conseguir chegar até o maior e mais perigoso
+                monstro de uma região. `,
             },
             {
                 nome: "Monster Hunter 3",
                 url: "https://www.gamulator.com/img/roms/monster-hunter-portable-3RD-psp-cover-japan.jpg",
+                descricao: `Monster Hunter Portable 3rd é a terceira parcela da franquia Monster Hunter,
+                desenvolvida pela Capcom para o PlayStation Portable`,
             },
             {
                 nome: "The Kingdom Hearts",
                 url: "https://www.gamulator.com/img/roms/kingdom-hearts-birth-by-sleep-psp-cover.jpg",
+                descricao: `Kingdom Hearts Birth by Sleep é um jogo eletrônico RPG de ação desenvolvido e
+                publicado pela Square Enix. É o sexto título da série Kingdom Hearts e foi lançado exclusivamente para
+                o PlayStation Portable em 9 de janeiro de 2010 no Japão, 7 de setembro na América do Norte e
+                em 10 de setembro de na região PAL.`,
             },
         ],
     },
@@ -221,38 +264,52 @@ var todosJogos = {
             {
                 nome: "Journey",
                 url: "https://gameroom.lt/1214/journey-collectors-edition.jpg",
+                descricao: `Journey é um jogo eletrônico independente desenvolvido pela Thatgamecompany para o PlayStation 3,
+                PlayStation 4 e Microsoft Windows. Foi lançado em 13 de março de 2012 na PlayStation Network`,
             },
             {
                 nome: "Horizon Zero Dawn",
                 url: "https://sm.ign.com/ign_br/game/h/horizon-ze/horizon-zero-dawn-1_xv24.jpg",
+                descricao: `Horizon Zero Dawn é um jogo eletrônico de RPG de ação pós-apocalíptico em um mundo aberto,
+                desenvolvido pela Guerrilla Games, apresentado em 2015 na conferencia da Sony na feira E3 e lançado em
+                28 de fevereiro de 2017 para o console PlayStation 4 e em 7 de agosto de 2020 para o sistema Microsoft Windows`,
             },
             {
                 nome: "Monster Hunter World",
                 url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRoSPNJHP4YgBG4WHK-df-zo25MJTciOvcT4ixrcxKmxKMpDWif4BBDEqGp7pss1mkmF0&usqp=CAU",
+                descricao: `Monster Hunter: World é um jogo de RPG eletrônico de ação desenvolvido e publicado pela Capcom.
+                O jogo faz parte da série Monster Hunter, foi lançado mundialmente em 26 de janeiro de 2018 para PlayStation
+                4 e Xbox One e em 9 de agosto de 2018 para Windows.`,
             },
             {
                 nome: "Monster Hunter IceBorn",
                 url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYjibtY_bxsr5RbGQKb4rH0QpLf5ErcAWzyA&usqp=CAU",
+                descricao: `Monster Hunter World: Iceborne é um pacote de expansão desenvolvido e publicado pela Capcom
+                para o RPG de ação de 2018 Monster Hunter: World.`,
             },
             {
                 nome: "Overcooked",
                 url: "https://smartcdkeys.com/image/data/products/Overcooked-PS4/cover/Overcooked-PS4-smartcdkeys-cheap-cd-key-cover.jpg",
-            },
-            {
-                nome: "Portal Knights",
-                url: "https://m.media-amazon.com/images/I/71-2rjtO3CL._AC_SX522_.jpg",
-            },
-            {
-                nome: "Overcooked 2",
-                url: "https://m.media-amazon.com/images/I/81T1dOwDD0L.jpg",
+                descricao: `Overcooked é um jogo caótico de cooperação na cozinha para um a quatro jogadores.
+                Trabalhando em equipe, você e seus colegas chefes de cozinha precisam preparar, cozinhar e servir uma
+                variedade de pedidos deliciosos antes que os clientes chatos saiam irritados. Afiem as facas e
+                tirem o pó dos seus uniformes de chefes de cozinha, não tem lugar para erros nem do tamanho de um cogumelo,
+                pois as "postas" são elevadas nestas cozinhas malucas!`,
             },
             {
                 nome: "The Last Guardian",
                 url: "https://m.media-amazon.com/images/I/6141ElAv+SS._AC_SX385_.jpg",
+                descricao: `The Last Guardian, chamado no Japão de Hitokui no Ōwashi Toriko,
+                é um jogo eletrônico de ação-aventura desenvolvido pela SIE Japan Studio e genDesign e
+                publicado pela Sony Interactive Entertainment. Foi lançado exclusivamente para PlayStation 4 em 6 de dezembro de 2016.`,
             },
             {
                 nome: "The Witness",
                 url: "https://static.wixstatic.com/media/c8fcd4_417391fb837f4384b05caf29d3ce4eb4~mv2.jpg/v1/fill/w_456,h_575,al_c,q_85/c8fcd4_417391fb837f4384b05caf29d3ce4eb4~mv2.jpg",
+                descricao: `The Witness é um jogo de mundo aberto para um jogador, com dezenas de locais para explorar e
+                mais de 500 puzzles para você solucionar. O jogo respeita você como um jogador inteligente e valoriza o seu tempo.
+                O gameplay vai direto ao ponto e cada puzzle contribui de maneira única com o conjunto da experiência.
+                Em resumo, este é um jogo repleto de ideias.`,
             },
         ],
     },
@@ -261,20 +318,29 @@ var todosJogos = {
         nome: "Online",
         jogos: [
             {
-                nome: "EuroTruck",
-                url: "https://cdn.cloudflare.steamstatic.com/steam/apps/227300/capsule_616x353.jpg?t=1656428921",
-            },
-            {
                 nome: "Nexomon",
                 url: "https://cdn.cloudflare.steamstatic.com/steam/apps/1191580/header.jpg?t=1661675530",
+                descricao: `Retorne à clássica captura de monstros, com uma história inédita,
+                personagens excêntricos e mais de 300 Nexomon únicos para capturar e treinar.
+                Entre para a Tamer's Guild (Guilda dos Domadores) e comece uma jornada épica para restaurar o
+                equilíbrio antes que toda esperança se perca…`,
             },
             {
                 nome: "Stardew Valley",
                 url: "https://cdn.cloudflare.steamstatic.com/steam/apps/440820/capsule_616x353.jpg?t=1581550226",
+                descricao: `O título traz elementos que lembram Minecraft e Terraria em um cenário rural típico da série
+                clássica Harvest Moon. O resultado é um jogo com história carismática, uma jogabilidade viciante e com
+                muito conteúdo para ser explorado. Fugindo da cidade moderna, o jogador herda uma fazenda na pacata
+                Stardew Valley e deve aprender a cultivar, criar animais e conviver em comunidade para construir laços em
+                sua nova vida no campo e até mesmo formar uma família, já que há a possibilidade de se casar com personagens
+                como Abigail e Leah`,
             },
             {
                 nome: "Terraria",
                 url: "https://cdn.akamai.steamstatic.com/steam/apps/105600/header.jpg?t=1590092560",
+                descricao: `Terraria é um jogo eletrônico RPG de ação-aventura independente produzido pela desenvolvedora de
+                jogos Re-Logic. Possui como características a exploração, artesanato, construção de estruturas e combate a
+                monstros perigosos em um mundo 2D gerado proceduralmente.`,
             },
         ],
     },
@@ -285,22 +351,33 @@ var todosJogos = {
             {
                 nome: "Empires & Puzzles",
                 url: "https://static-cdn.jtvnw.net/ttv-boxart/510811_IGDB-285x380.jpg",
-            },
-            {
-                nome: "Guardian Tales",
-                url: "https://static-cdn.jtvnw.net/ttv-boxart/517357_IGDB-285x380.jpg",
+                descricao: `Empires & Puzzles é uma lufada de ar fresco nos jogos RPG, juntando batalhas de combinar 3
+                à construção de uma fortaleza poderosa - acrescido de emocionantes duelos de PvP.`,
             },
             {
                 nome: "PunBall",
                 url: "https://imag.malavida.com/mvimgbig/download-fs/punball-33990-0.jpg",
+                descricao: `PunBall é um jogo que combina RPG e características "roguelike" para mergulhar você
+                no famoso universo de Archero, porém com um enredo completamente novo.
+                Aqui você estará encarregado de ajudar um jovem mago em sua jornada em defesa da humanidade contra toda
+                a sorte de diferentes inimigos.`,
             },
             {
                 nome: "Random Dice",
                 url: "https://static-cdn.jtvnw.net/ttv-boxart/517746_IGDB-272x380.jpg",
+                descricao: `Tudo pronto para um embate Dadônico?
+                Comande sua equipe de dados superpoderosos!
+                Faça fusões, suba de nível e defenda seu território contra ondas sem fim de monstros chefes!`,
             },
             {
                 nome: "Survivor.io",
                 url: "https://www.writerparty.com/wp-content/uploads/9DDF0969-DE0D-4D41-8DA1-AA24A55EB08E.jpeg.webp",
+                descricao: `Zumbis perigosos estão atacando toda a cidade! A cidade está em perigo!
+                Despertado pelo julgamento dos sonhos, você não tem escolha a não ser assumir o manto heróico de salvar a cidade!
+                Como um guerreiro humano com potencial ilimitado, você e outros sobreviventes terão que pegar suas armas
+                e lutar contra esses zumbis malignos e perigosos!
+                A horda supera em muito você - qualquer deslize e você será pego em apuros!
+                Diante da crise, você deve encontrar uma maneira de sobreviver!`,
             },
         ],
     },
